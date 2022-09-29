3a77f69f388c125372213612329300acc28c5754
Korben Gao <korben.au@gmail.com>
2022-09-29 21:31:25 +1000

    initial commit

---
+++ 1-kafka-cli/2-kafka-console-consumer.sh
@@ -0,0 +1,16 @@
+# Replace "kafka-console-consumer" 
+# by "kafka-console-consumer" or "kafka-console-consumer.bat" based on your system # (or bin/kafka-console-consumer or bin\windows\kafka-console-consumer.bat if you didn't setup PATH / Environment variables)
+
+kafka-console-consumer 
+
+# consuming
+kafka-console-consumer --bootstrap-server 192.168.1.101:29092 --topic first_topic
+
+# other terminal
+kafka-console-producer --bootstrap-server 192.168.1.101:29092 --topic first_topic
+
+# consuming from beginning
+kafka-console-consumer --bootstrap-server 192.168.1.101:29092 --topic first_topic --from-beginning
+
+# display key, values and timestamp in consumer
+kafka-console-consumer --bootstrap-server 192.168.1.101:29092 --topic first_topic --formatter kafka.tools.DefaultMessageFormatter --property print.timestamp=true --property print.key=true --property print.value=true --from-beginning
